f553cb22f1f405f400f17ae083d18f579d408623
Yuxi Wang <yuxi.wangcd@gmail.com>
2020-11-19 11:29:10 -0700

    Remove unused deps

--- src/webgl/store/selectors.ts
+++
@@ -1,8 +0,0 @@
-import { createSelector, Selector } from 'reselect'
-import prop from 'ramda/es/prop'
-import { AppState, CameraState } from '.'
-
-export const camera: Selector<AppState, CameraState> = prop('camera')
-export const rotation = createSelector(camera, prop('rotation'))
-export const scaleVec = createSelector(camera, prop('scaleVec'))
-export const translation = createSelector(camera, prop('translation'))
